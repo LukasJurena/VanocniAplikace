56a9952899493a90f06c7fe024ab2d079c772c95
LukasJurena <jurenalukash@gmail.com>
2025-12-15 09:20:05 +0100

    uprava

--- VanocniAplikace/Program.cs
+++ VanocniAplikace/Program.cs
@@ -12,9 +12,29 @@ namespace VanocniAplikace
         {
             Console.WriteLine("Ahoj jsem pomocník od santy, co si od něj přeješ?");
             string prani = Console.ReadLine();
-            Console.WriteLine("Děkuji, santovi předám tvé přání: " + prani);
+            Console.WriteLine("To je vše? Nechceš si přát ještě něco? [ano/ne]");
+            while (true)
+            {
+                string odpoved = Console.ReadLine().ToLower();
+                if (odpoved == "ano")
+                {
+                    Console.WriteLine("Tak co ještě chceš?");
+                    string dalsiPrani = Console.ReadLine();
+                    prani += ", " + dalsiPrani;
+                    Console.WriteLine("Chceš si přát ještě něco?");
+                }
+                else if (odpoved == "ne")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Prosím odpověz 'ano' nebo 'ne'. Chceš si přát ještě něco?");
+                }
+            }
+            Console.WriteLine("Děkuji, santovi předám tvé přání");
             Console.WriteLine("Přeji ti krásné Vánoce!");
-            Console.WriteLine("..Santa si tvé přáníčko přečetl, ale jelikož si přes rok zlobil, tak dostaneš jenom uhlí Ho Ho ho.");
+            Console.WriteLine("..Santa si tvé přáníčko přečetl, ale jelikož si přes rok zlobil, tak dostaneš jenom uhlí, Ho Ho ho.");
         }
     }
 }
