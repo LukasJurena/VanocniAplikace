a4eb66eb07a8176a80324a06e4301d5a5f7bb8c9
LukasJurena <jurenalukash@gmail.com>
2025-12-15 09:16:31 +0100

    Přidejte soubory projektu.

---
+++ VanocniAplikace/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VanocniAplikace
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ahoj jsem pomocník od santy, co si od něj přeješ?");
+            string prani = Console.ReadLine();
+            Console.WriteLine("Děkuji, santovi předám tvé přání: " + prani);
+            Console.WriteLine("Přeji ti krásné Vánoce!");
+            Console.WriteLine("..Santa si tvé přáníčko přečetl, ale jelikož si přes rok zlobil, tak dostaneš jenom uhlí Ho Ho ho.");
+        }
+    }
+}
